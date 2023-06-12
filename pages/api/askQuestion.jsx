81de04d559896daab7efddd5d5b544e54f1313b0
David-mwas <dmwas704@gmail.com>
2023-06-12 08:26:50 +0300

    changed input

--- pages/api/askQuestion.jsx
+++ pages/api/askQuestion.jsx
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         user:{
             _id:"ChatGPT",
             name:"ChatGPT",
-            avatar:"voicegpt.jpeg"
+            avatar:"/voicegpt.png"
         }
     }
 
