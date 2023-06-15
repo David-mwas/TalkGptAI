7b04c83c2afc165fb66c1fccfddef173c0e6fa50
David-mwas <dmwas704@gmail.com>
2023-06-16 02:21:52 +0300

    code cleaning , refactoring and updating search input validation

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,16 +1,59 @@
+'use client'
+import { useRef, useState,useEffect } from "react";
+import { FaCopy } from "react-icons/fa";
 
 function Message({message}){
+    const [copied,setCopied]=useState(false);
+    const divRef=useRef(null);
     const isChatGPT = message.user.name === "ChatGPT";
+    // console.log(isChatGPT&&message.text)
+    const handleCopyClick = () => {
+        const textToCopy = divRef?.current.innerText;
+        console.log(textToCopy)
+        if(textToCopy){
+           navigator.clipboard.writeText(textToCopy)
+           .then(()=>{
+            setCopied(true)
+            
+           })
+            .catch((error)=>{
+                console.error('copy failed', error)
+            })
+            
+        
+    };
+
+      
+       
+   }
+   setTimeout(()=>{setCopied(false),2000});
+   
+//    useEffect(() => {
+//     let timeoutId;
+//     if (copied){
+//        timeoutId =  setTimeout(()=>{setCopied(false),2000});
+//     }
+  
+//     return () => {
+//       clearTimeout(timeoutId);
+
+//     }
+//   }, [copied])
     return (
-        <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>
-        <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
+        <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`} ref={isChatGPT ? divRef : null}>
+        <div className="flex space-x-5 px-10 max-w-2xl mx-auto relative" >
             <img src={message.user.avatar} alt="" className="h-12 w-12 rounded-full object-cover"/>
-            <p className="pt-1 text-sm">
-                {message.text}
-            </p>
+           <div  className=" flex flex-row-reverse items-start space-x-4 gap-3 justify-between " >
+           
+               {isChatGPT && <button onClick={handleCopyClick} className="absolute right-1">
+                       {copied ? 'copied!' : <FaCopy className="h-6 w-6 text-gray-800 align-top text-end hover:opacity-75"/>}</button>}
+                       <p className="pt-1 text-sm">
+           {message.text}
+               </p>
+           </div>
         </div>
         </div>
     )
-}
+} 
 
 export default Message
