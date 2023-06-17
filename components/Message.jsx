5a8ca3f72fad26a36e1d4e1d8db9e4bf80f85a47
David-mwas <dmwas704@gmail.com>
2023-06-17 04:17:34 +0300

    added google synthesis voice to read chat completions

--- components/Message.jsx
+++ components/Message.jsx
@@ -4,7 +4,7 @@ import { FaCopy, FaVolumeUp } from "react-icons/fa";
 import { useSpeechSynthesis } from "react-speech-kit";
 
 function Message({ message }) {
-    const { speak } = useSpeechSynthesis();
+    const { speak,speaking,voices } = useSpeechSynthesis();
     const [copied,setCopied] = useState(false);
     const divRef = useRef(null);
     const isChatGPT = message.user.name === "ChatGPT";
@@ -40,15 +40,18 @@ function Message({ message }) {
     const handleSpeek = () => {
         
         if (isChatGPT) {
-            speak({text:message.text});
+            if (!speaking) {
+               speak({text:message.text, voice:voices[4]}); 
+            }
         }
+        console.log(voices[4])
         
     }
 
     return (
         <div className={`w-[100%] relative py-5 px-1 rounded-lg text-white ${isChatGPT && "bg-[#434654] md:max-w-[84%]"}`} ref={isChatGPT ? divRef : null}>
         <div className="flex space-x-5 px-10 max-w-2xl mx-auto relative" >
-            <img src={message.user.avatar} alt="" className="h-12 w-12 rounded-xl shadow-lg shadow-black object-cover"/>
+            <img src={message.user.avatar} alt="" className={`h-12 w-12 rounded-xl shadow-lg shadow-black object-cover ${isChatGPT && speaking && "shadow-xl shadow-green-400"}`}/>
            <div  className=" flex flex-row-reverse items-start space-x-4 gap-3 justify-between " >
            
                {isChatGPT && <button onClick={handleCopyClick} className="absolute text-[12px] md:text-sm right-2 md:right-[-20px] top-[-11px]">
@@ -59,7 +62,7 @@ function Message({ message }) {
                     {isChatGPT && <button onClick={()=>{handleSpeek()}}
                         className="absolute text-[12px] md:text-sm right-2 md:right-[-20px] bottom-[3px]">
 
-                        <FaVolumeUp className="h-5 w-5  text-gray-400 align-top text-end hover:opacity-75 "/>
+                        <FaVolumeUp className={`h-5 w-5  text-gray-400 align-top text-end hover:opacity-75  ${speaking && "text-green-500"}`}/>
                         </button>}
                        <p className="pt-1 text-sm text-gray-300">
            {message.text}
